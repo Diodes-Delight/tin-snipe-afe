23a0afe72fb5c6360f89e49b8d062e862bbdb8e5
Elen Eisendle <ln@calcifer.ee>
2025-08-27 14:20:48 +0200

    fix gain and attenuation switching code

--- attenuator-amplifier/main.c
+++ attenuator-amplifier/main.c
@@ -41,8 +41,9 @@ void cycle_relays()
 {
     const uint32_t relay_actuation_time = 80000; // 3ms / 42ns (24 MHz) rounded up
     /*
-        In order of MSB first:
-        b0, b1: gain selection
+        In order of LSB first:
+        b0: B
+        b1: A
         b2: R3+S
         b3: R3+R
         b4: R1+R
@@ -51,44 +52,44 @@ void cycle_relays()
         b7: R2+S
 
         Sequence: 
-        1. R1+R,   R2+S, R3+R     for 1000:1
-        2. (R1+R), R2+R, R3+S     for 100:1
-        3. (R1+R), R2+S, (R3+S)   for 10:1
-        4. R1+S,   GS_A=0, GS_B=1 for 1:1
-        5. (R1+S), GS_A=1, GS_B=0 for 1:10
-        6. (R1+S), GS_A=1, GS_B=1 for 1:100
+        1. R1+S,   R2+R, R3+S     for 1000:1 - works
+        2. (R1+S), R2+S, R3+R     for 100:1
+        3. (R1+S), R2+R, (R3+R)   for 10:1
+        4. R1+R,   GS_A=0, GS_B=1 for 1:1
+        5. (R1+R), GS_A=1, GS_B=0 for 1:10
+        6. (R1+R), GS_A=1, GS_B=1 for 1:100
     */
-    if (gCurrentSequence == 0) {
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01011001});
+    if (gCurrentSequence == 0) { // 1000:1, gain set to 10x because of lack of high voltage test equipment, effectively 100:1
+        set_shift_register_state((uint8_t[]){0b00000011, 0b01100101});
         delay_cycles(relay_actuation_time);
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01000000});
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00000001});
         gCurrentSequence = 1;
-    } else if (gCurrentSequence == 1) {
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01100010});
+    } else if (gCurrentSequence == 1) { // 100:1
+        set_shift_register_state((uint8_t[]){0b00000011, 0b10001010});
         delay_cycles(relay_actuation_time);
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01000000});
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00000010});
         gCurrentSequence = 2;
-    } else if (gCurrentSequence == 2) {
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01000001});
+    } else if (gCurrentSequence == 2) { // 10:1
+        set_shift_register_state((uint8_t[]){0b00000011, 0b01000010});
         delay_cycles(relay_actuation_time);
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01000000});
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00000010});
         gCurrentSequence = 3;
-    } else if (gCurrentSequence == 3) {
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01000100});
+    } else if (gCurrentSequence == 3) { // 1:1
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00010010});
         delay_cycles(relay_actuation_time);
-        set_shift_register_state((uint8_t[]){0b11000000, 0b01000000});
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00000010});
         gCurrentSequence = 4;
-    } else if (gCurrentSequence == 4) {
-        set_shift_register_state((uint8_t[]){0b11000000, 0b10000000});
+    } else if (gCurrentSequence == 4) { // 1:10
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00000001});
         gCurrentSequence = 5;
-    }  else if (gCurrentSequence == 5) {
-        set_shift_register_state((uint8_t[]){0b11000000, 0b11000000});
+    }  else if (gCurrentSequence == 5) { // 1:100
+        set_shift_register_state((uint8_t[]){0b00000011, 0b00000011});
         gCurrentSequence = 0;
     }
 }
 
 int main(void)
-{
+    {
     SYSCFG_DL_init();
 
     NVIC_EnableIRQ(GPIO_SWITCHES_INT_IRQN);
@@ -100,27 +101,17 @@ int main(void)
         if (gIntTriggered == 1) {
             cycle_relays();
             gIntTriggered = 0;
-
-            for (int i=0; i <= gCurrentSequence; i++) {
-                DL_GPIO_setPins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_1_PIN);
-                delay_cycles(12000000);
-                DL_GPIO_clearPins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_1_PIN);
-            }
         }
     }
 }
 
 void GPIOA_IRQHandler(void)
 {
-    /* If SW is high, turn the LED off */
     if (DL_GPIO_readPins(
             GPIO_SWITCHES_PORT, GPIO_SWITCHES_USER_SWITCH_1_PIN)) {
-        //DL_GPIO_setPins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_1_PIN);
         __NOP();
     }
-    /* Otherwise, turn the LED on */
     else {
-        //DL_GPIO_clearPins(GPIO_LEDS_PORT, GPIO_LEDS_USER_LED_1_PIN);
         gIntTriggered = 1;
     }
 }
